3b35fbb58b295d65a50388b81bce1fe70a537305
Opal Symes <code@opal.codes>
2018-11-10 07:36:38 +1300

    Importing changes

--- archivelib-sys2/c-lib/src/r_expand/fn255.cpp
+++ archivelib-sys2/c-lib/src/r_expand/fn255.cpp
@@ -7,10 +7,12 @@ void RExpand::fn255() {
   bits_to_load219 = get_bits(CONST_N143_IS_9);
   if (bits_to_load219 == 0) {
     byte_or_run_length203 = get_bits(CONST_N143_IS_9);
-    for (run_start226 = 0; run_start226 < CONST_N141_IS_511; run_start226++)
+    for (run_start226 = 0; run_start226 < CONST_N141_IS_511; run_start226++) {
       data->dat_arr180[run_start226] = 0;
-    for (run_start226 = 0; run_start226 < CONST_N148_IS_4096; run_start226++)
+    }
+    for (run_start226 = 0; run_start226 < CONST_N148_IS_4096; run_start226++) {
       data->dat_arr240[run_start226] = byte_or_run_length203;
+    }
   } else {
     run_start226 = 0;
     while (run_start226 < bits_to_load219) {
@@ -18,28 +20,33 @@ void RExpand::fn255() {
       if (byte_or_run_length203 >= CONST_N145_IS_19) {
         _283 = 1U << 7;
         do {
-          if (data->bits182 & _283)
+          if (data->bits182 & _283) {
             byte_or_run_length203 = data->dat_arr190[byte_or_run_length203];
-          else
+          } else {
             byte_or_run_length203 = data->dat_arr189[byte_or_run_length203];
+          }
           _283 >>= 1;
         } while (byte_or_run_length203 >= CONST_N145_IS_19);
       }
       read_bits(data->dat_arr181[byte_or_run_length203]);
       if (byte_or_run_length203 <= 2) {
-        if (byte_or_run_length203 == 0)
+        if (byte_or_run_length203 == 0) {
           byte_or_run_length203 = 1;
-        else if (byte_or_run_length203 == 1)
+        } else if (byte_or_run_length203 == 1) {
           byte_or_run_length203 = (int16_t)(get_bits(4) + 3);
-        else
+        } else {
           byte_or_run_length203 = (int16_t)(get_bits(CONST_N143_IS_9) + 20);
-        while (--byte_or_run_length203 >= 0)
+        }
+        while (--byte_or_run_length203 >= 0) {
           data->dat_arr180[run_start226++] = 0;
-      } else
+        }
+      } else {
         data->dat_arr180[run_start226++] = (uint8_t)(byte_or_run_length203 - 2);
+      }
     }
-    while (run_start226 < CONST_N141_IS_511)
+    while (run_start226 < CONST_N141_IS_511) {
       data->dat_arr180[run_start226++] = 0;
+    }
     fn258(CONST_N141_IS_511, data->dat_arr180, 12, data->dat_arr240,
           CONST_N148_IS_4096);
   }
